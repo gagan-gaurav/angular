bb21c1e1356b9909df008e60b3fdeee776dcc308
gagan-gaurav <iamgagangaurav@gmail.com>
2022-05-15 22:35:23 +0530

    commit

---
+++ wordle/src/app/wordle.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+ 
+ @Component({
+	 selector: 'wordle',
+	 templateUrl: '/wordle.component.html',
+	 styleUrls: ['./wordle.component.scss'],
+ })
+ export class Wordle {
+
+ }
